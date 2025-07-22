4e37dd340b8de5db2044c73b9472fe615b32ae8b
Naveen-144 <naveenk144410@gmail.com>
2025-07-22 11:44:44 +0530

    operators completed

--- operators.java
+++ operators.java
@@ -8,6 +8,33 @@ public class operators {
         int b=5;
         b^=6;
         System.out.println(b);
-    }
+
+        //Relational  operators
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+
+        //logial operator and bitwise operator
+        int c=10;
+        int d=10;
+        int e=9;
+        int f=2;
+        System.out.println(c&d);
+        System.out.println(c|d);
+        System.out.println((c==d)&&(e>f));
+        System.out.println((c==d)||(e<f));
+        System.out.println(~c);
+
+        // tereneray operator
+        //syntax (condition)?(statement 1):(statement 2)
+        System.out.println((c==d)?"true":2);
+
+        }
+        }  
+
+
     
-}
+    
