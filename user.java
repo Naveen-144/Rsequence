5dc9a2a95d151c0cc53ac5464d7338522d51c58a
Naveen-144 <naveenk144410@gmail.com>
2025-07-22 12:26:29 +0530

    input

--- user.java
+++ user.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class user {
     public static void main(String[] args) {
         Scanner nav = new Scanner(System.in);
-        int a =nav.nextInt();
+        double a =nav.nextDouble()
         System.out.println(a);
         nav.close();
 
